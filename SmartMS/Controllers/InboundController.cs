a1cf57caee564e0018711ffdadcbf59f9f34b094
Feiyingx <feiyingx@gmail.com>
2014-02-05 16:00:48 -0800

    initial setup

---
+++ SmartMS/Controllers/InboundController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SmartMS.Controllers
+{
+    public class InboundController : Controller
+    {
+        public ActionResult Index()
+        {
+            String incomingNumber = Request.QueryString["From"];
+            String incomingMessage = Request.QueryString["Body"];
+            return View();
+        }
+
+    }
+}
