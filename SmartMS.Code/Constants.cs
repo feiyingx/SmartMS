848e94b78f779d60ddd11daa0233b3ae1fd550ed
Feiyingx <feiyingx@gmail.com>
2014-02-05 17:55:19 -0800

    added tip functionality

--- SmartMS.Code/Constants.cs
+++ SmartMS.Code/Constants.cs
@@ -10,7 +10,18 @@ namespace SmartMS.Code
     {
         public class ContentType
         {
-            public static String XML = "text/xml";
+            public const String XML = "text/xml";
+        }
+
+        public class SMSCommands
+        {
+            public const String Menu = "menu";
+            public const String Navigation = "nav";
+            public const String Tip = "tip";
+            public const String Bart = "bart";
+            public const String Score = "score";
+            public const String Food = "food";
+            public const String Set = "set";
         }
     }
 }
--- SmartMS/Controllers/InboundController.cs
+++ SmartMS/Controllers/InboundController.cs
@@ -11,20 +11,78 @@ namespace SmartMS.Controllers
     {
         public ActionResult Index()
         {
+            //Get the incoming message's information passed in by Twilio as a query string
             String incomingNumber = Request.QueryString["From"];
             String incomingMessage = Request.QueryString["Body"];
 
-            if (incomingMessage != null)
+            //Check to make sure we have a message and a number
+            if (incomingMessage != null && incomingNumber != null)
             {
-                incomingMessage = incomingMessage.Trim();
-                if (incomingMessage.ToLower() == "menu")
+                //Parse command
+                //Normalize the message string by removing extra spaces and change to lower case
+                incomingMessage = incomingMessage.Trim().ToLower();
+                //Try to find the command of the message, if we have trouble parsing it then return invalid command error message
+                int commandEndIndex = incomingMessage.IndexOf(' ');
+                if (commandEndIndex < 0)
                 {
-                    String responseXml = String.Format("<Response><Message>I'm still a baby computer. I only know how to:</Message></Response>");
-                    return this.Content(responseXml, Constants.ContentType.XML);
+                    String invalidCommandMessage = String.Format("<Response><Message>I couldn't understand your command. Please text 'menu' to see the list of commands.</Message></Response>");
+                    return this.Content(invalidCommandMessage, Constants.ContentType.XML); 
+                }
+
+                //Extract the command string and update the remaining part of the message
+                String command = incomingMessage.Substring(0, commandEndIndex);
+                String remainingMessage = incomingMessage.Substring(commandEndIndex + 1).Trim(); //Trim again to remove any extra spaces
+                switch (command)
+                {
+                    case Constants.SMSCommands.Bart:
+                        break;
+                    case Constants.SMSCommands.Food:
+                        break;
+                    case Constants.SMSCommands.Menu:
+                        break;
+                    case Constants.SMSCommands.Navigation:
+                        break;
+                    case Constants.SMSCommands.Score:
+                        break;
+                    case Constants.SMSCommands.Set:
+                        break;
+                    case Constants.SMSCommands.Tip:
+                        return ProcessTip(remainingMessage);
+                    default:
+                        break;
                 }
             }
-            String responseXml2 = String.Format("<Response><Message>Booya! I know your number: {0}</Message></Response>", incomingNumber);
-            return this.Content(responseXml2, Constants.ContentType.XML);
+            
+            //If we don't have any incoming message or number then return some kind of notice
+            String errorMessage = String.Format("<Response><Message>Uh oh, I don't think we are meant to be. So let's just let it be.</Message></Response>");
+            return this.Content(errorMessage, Constants.ContentType.XML);
+        }
+
+        private ActionResult ProcessTip(String message)
+        {
+            //Tip command strings should look like:
+            //"tip xxx.xx"
+            //The message parameter should already have the command part removed
+            //So let's try to extract the amount that we are calculating the tip for
+            decimal billAmount = 0;
+            bool canParseBillAmount = Decimal.TryParse(message, out billAmount);
+            //Default to an error message
+            String resultMessage = "I couldn't read the amount, please text me again in the example format 'tip 120.68'.";
+            if (canParseBillAmount)
+            {
+                //Format tip amounts in currency format
+                resultMessage = String.Format(
+@"18% - {0:c}
+15% - {1:c}
+13% - {2:c} :(
+10% - {3:c} :'(", billAmount*.18m, billAmount*.15m, billAmount*.13m, billAmount*.10m);
+            }
+            
+            return this.Content(BuildTwiML(resultMessage), Constants.ContentType.XML); 
+        }
+        private String BuildTwiML(String response)
+        {
+            return String.Format("<Response><Message>{0}</Message></Response>", response);
         }
     }
 }
